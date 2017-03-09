7cb1e9d2697bd3b3bf51aa9fbfb48731ca9d9920
Bradley Lowenstein <lowensteinbs@Bradleys-MacBook-Pro.local>
2017-03-09 18:17:27 -0500

    Added main class to separate logic

---
+++ src/com/assessment/utilities/AssessmentMain.java
@@ -0,0 +1,31 @@
+package com.assessment.utilities;
+
+import com.assessment.bl.WriteFile;
+import com.assessment.dc.Constants;
+import com.assessment.dc.URLResponse;
+import com.google.gson.JsonSyntaxException;
+
+public class AssessmentMain {
+	public static void main(String args[]) throws Exception {
+
+        try {
+            Assessment assessment = new Assessment();
+            URLResponse urlResponse[] = assessment.execute(args);
+            urlResponse = assessment.getURLResponse(urlResponse);
+
+            try {
+                WriteFile.writeFile(urlResponse, assessment.isJsonArray());
+            } catch (Exception e) {
+                throw new Exception("Error writing file");
+            }
+
+            System.out.println("Please see " + Constants.OUTPUT_FILENAME + " for results.");
+        }
+        catch (JsonSyntaxException jsonSyntaxException) {
+            throw new Exception("Invalid json syntax. " + Constants.VALID_JSON_FORMAT);
+        }
+        catch (Exception e) {
+            throw new Exception(e);
+        }
+    }
+}
